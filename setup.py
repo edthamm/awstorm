dba92d55334143d08a9357747d99d495736b7c37
Eduard Thamm <eduard.thamm@thammit.at>
2019-05-11 13:18:20 +0200

    Improve setup py

--- setup.py
+++ setup.py
@@ -1,24 +1,41 @@
 import pathlib
 from setuptools import setup
 
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "Readme.md").read_text()
+PROJECT = "awstorm"
+VERSION = "0.0.1"
+
+try:
+    HERE = pathlib.Path(__file__).parent
+    README = (HERE / "Readme.md").read_text()
+except IOError:
+    README = ""
+
 
 setup(
-    name="awstorm",
-    version="0.0.1",
-    description="A Package to generate AWS project skeletons",
+    name=PROJECT,
+    version=VERSION,
+
+    description="A Package to improve interaction with AWS Cloudformation",
     long_description=README,
     long_description_content_type="text/markdown",
+
     url="https://github.com/edthamm/awstorm",
+
     author="Eduard Thamm",
     author_email="eduard.thamm@thammit.at",
+
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
+
+    platforms=['Any'],
+
+    install_requires=['cliff', 'boto3'],
+
     packages=["awstorm"],
-    include_package_data=True
+    include_package_data=True,
+    zip_safe=False,
 )
